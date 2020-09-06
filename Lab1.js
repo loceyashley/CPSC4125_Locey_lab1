128188d294960f14d98c05615a45156593870d03
loceyashley <locey_ashley@columbusstate.edu>
2020-09-06 19:45:04 +0000

    added

--- Lab1.js
+++ Lab1.js
@@ -1,17 +1,31 @@
 arrayOfEmails = [];
 
+//Add emails to the list 
 function addEmail(){
     var Email = document.getElementById('validationTooltip02');
     arrayOfEmails.push(Email.value);
-
-    for(var i = 0; i < arrayOfEmails.length; i++){
-        document.write("<br>"+arrayOfEmails[i]);
-    }
-    
-    Email.value="";
-
+    //update();
+    printList();
 }
+
+//print list 
+function printList(){
+    for(var i = 0; i < arrayOfEmails.length; i++){
+       //document.write("<br>"+arrayOfEmails[i]);
+       document.getElementById("result").innerHTML = arrayOfEmails[i];
+    }
+}
+
+//update list
+function update(){
+    arrayOfEmails = arrayOfEmails+addEmail();
+}
+//remove fromlist 
 function removeEmail(){
+    if (arrayOfEmails.length == 0)
+    {
+        document.write("There are no emails on the list too delete.");
+    }
     arrayOfEmails.pop('validationTooltip02')
 }
 
