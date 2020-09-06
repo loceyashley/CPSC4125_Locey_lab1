b7355f1d1abea63ed12c30506b2ae56c0c476e6d
loceyashley <locey_ashley@columbusstate.edu>
2020-09-06 14:56:45 +0000

    added

--- Lab1.js
+++ Lab1.js
@@ -1,4 +1,22 @@
-<script>
+<script type="text/javascript">
+
+arrayOfEmails = [];
+
+function addEmail(){
+    Var Email = document.getElementById('validationTooltip02');
+    arrayOfEmails.push(Email.value);
+    Email.value="";
+
+}
+
+function printEmail() {
+  document.getElementById("values").innerHTML = values.join(", ");
+}
+
+function removeEmail(){
+    arrayOfEmails.pop(validationTooltip02)
+}
+
 
 
 </script>
